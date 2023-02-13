d7fc2bfa9b0ace2804c2c4b8905100cd8321f783
mohiandipta <mohianmostofa@gmail.com>
2023-02-13 16:33:22 +0600

    new page

---
+++ src/pages/newnote.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+import { Head } from "next/document";
+import React from "react";
+
+type Props = {};
+
+const newnote = (props: Props) => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b">
+        form to add new note
+      </main>
+    </>
+  );
+};
+
+export default newnote;
