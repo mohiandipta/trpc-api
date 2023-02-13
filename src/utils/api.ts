e2d5ce3f6d5379021a5751ea8ff3b8a2bbf305ca
mohiandipta <mohianmostofa@gmail.com>
2023-02-13 15:04:47 +0600

    connected prisma with mysql

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -18,22 +18,10 @@ const getBaseUrl = () => {
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
-/** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
   config() {
     return {
-      /**
-       * Transformer used for data de-serialization from the server.
-       *
-       * @see https://trpc.io/docs/data-transformers
-       **/
       transformer: superjson,
-
-      /**
-       * Links used to determine request flow from client to server.
-       *
-       * @see https://trpc.io/docs/links
-       * */
       links: [
         loggerLink({
           enabled: (opts) =>
@@ -46,24 +34,9 @@ export const api = createTRPCNext<AppRouter>({
       ],
     };
   },
-  /**
-   * Whether tRPC should await queries when server rendering pages.
-   *
-   * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
-   */
   ssr: false,
 });
 
-/**
- * Inference helper for inputs.
- *
- * @example type HelloInput = RouterInputs['example']['hello']
- **/
 export type RouterInputs = inferRouterInputs<AppRouter>;
 
-/**
- * Inference helper for outputs.
- *
- * @example type HelloOutput = RouterOutputs['example']['hello']
- **/
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
