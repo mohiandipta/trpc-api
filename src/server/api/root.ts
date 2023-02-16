8da61f2fb40e635a96962e90457198064b4aafb8
mohiandipta <mohianmostofa@gmail.com>
2023-02-16 12:56:43 +0600

    api added into component

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "./trpc";
 import { subscribeRouter } from "./routers/subscribe";
+import { notesRouter } from "./routers/mynotes";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { subscribeRouter } from "./routers/subscribe";
  */
 export const appRouter = createTRPCRouter({
   subscribe: subscribeRouter,
+  mynotes: notesRouter
 });
 
 // export type definition of API
